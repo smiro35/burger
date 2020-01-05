7b3774aab6162bf3a6c20a8fd2917ef62e69208a
Shakur <miiro.shakur@gmail.com>
2020-01-05 18:32:56 -0500

    code working, but handlebars still failing to display

---
+++ .history/db/seeds_20200105155559.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('whooper');
+INSERT INTO burgers (name) VALUES ('Bigmac');
+INSERT INTO burgers (name, devoured) VALUES ('chicken', true);
+INSERT INTO burgers (name, devoured) VALUES ('beef', false);
+
+
---
+++ .history/db/seeds_20200105160111.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('whooper');
+INSERT INTO burgers (burger_name) VALUES ('bigmac');
+INSERT INTO burgers (burger_name, devoured) VALUES ('whooper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bigmac', false);
+
+
